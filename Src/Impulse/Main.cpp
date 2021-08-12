be26284fca33e56c090d49fe7e28c189529bc59c
VacuityBox <69965495+VacuityBox@users.noreply.github.com>
2021-08-12 18:58:12 +0200

    Add project files

---
+++ Src/Impulse/Main.cpp
@@ -0,0 +1,14 @@
+#include "PCH.hpp"
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+auto WINAPI wWinMain (
+    _In_     HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_     LPWSTR    lpCmdLine,
+    _In_     int       nShowCmd
+) -> int
+{
+    return 0;
+}
---
+++ Src/Impulse/PCH.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+/// Windows libraries
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
